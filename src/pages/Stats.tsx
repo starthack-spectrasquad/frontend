bc107e040ae2ea27b935590e339671b1bbbdaf55
BenEliasMartin <martinbenelias@gmail.com>
2024-03-21 13:27:43 +0100

    stats page mostly

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,7 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import Container from '../components/Container';
 import { IonIcon, useIonRouter, IonBackButton } from '@ionic/react';
-import { trendingUp, arrowForwardCircle } from 'ionicons/icons';
+import { trendingUp, arrowForwardCircle, arrowDownCircleOutline } from 'ionicons/icons';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, AreaChart, Area } from 'recharts';
 
 
@@ -93,6 +93,17 @@ const data2 = [
         c: 2100,
     },
 ];
+
+const dailyData = [
+    { name: 'Allergenes', value: 80, },
+    { name: 'Proteins', value: 10, },
+    { name: 'Vitamins', value: 30, },
+    { name: 'Amino Acids', value: 50, },
+    { name: 'Electrolytes', value: 80, },
+    { name: 'Bodybuilding', value: 100, },
+];
+
+
 const toPercent = (decimal: number, fixed: number = 0): string => `${Math.round(decimal * 100 * Math.pow(10, fixed)) / Math.pow(10, fixed)}%`;
 
 const getPercent = (value: number, total: number): string => {
@@ -104,89 +115,124 @@ const getPercent = (value: number, total: number): string => {
 
 const Stats: React.FC = () => {
     return (
-        <IonContent className='bg-gray-100'>
-            <div className='m-0 p-0'>
+        <IonPage>
+            <IonContent className='bg-gray-100'>
+                <div className='m-0 p-0'>
 
 
-                <div className='bg-gray-100 min-h-screen px-2 py-4'>
-                    <div className='flex items-center jusitify-between'>
-                        <IonBackButton defaultHref="home" ></IonBackButton>
-                        <div></div>
-                    </div>
-                    <IonHeader className='mt-5 mx-6'>
-                        <IonTitle size="large">Statistics</IonTitle>
-                    </IonHeader>
-
-
-                    <Container>
-
-                        <div className="bg-white rounded-xl p-7">
-
-                            <h2 className='font-bold text-2xl mb-10'>Daily statistics</h2>
+                    <div className='bg-gray-100 min-h-screen px-2 py-4'>
+                        <div className='flex items-center jusitify-between'>
+                            <IonBackButton defaultHref="home" ></IonBackButton>
+                            <div></div>
                         </div>
-                    </Container>
-                    <Container>
-                        <div className='flex justify-start items-center mb-6'>
-                            <IonIcon icon={trendingUp} className='mr-2' />
-                            <h2 className="text-2xl font-bold text-gray-800">Trend</h2>
+                        <IonHeader className='mt-5 mx-6'>
+                            <IonTitle size="large">Statistics</IonTitle>
+                        </IonHeader>
 
 
-                        </div>
+                        <Container>
+
+                            <div className="bg-white rounded-xl p-7">
+                                <h1 className='font-bold text-2xl'>Health score</h1>
+                                <div className='flex items-center justify-start'>
+                                    <h2 className="text-4xl font-black text-indigo-500">67</h2>
+                                    <h2 className="text-4xl font-black text-gray-800">/100</h2>
+                                </div>
+
+                                <div className="max-w-md mx-auto my-2 p-6 bg-white rounded-3xl">
+                                    {dailyData.map((e: any, index: number): any => {
+                                        let customClass = "h-6 flex items-center justify-center"
+                                        if (e.value > 80 && e.value <= 120)
+                                            customClass += " bg-indigo-900"
+                                        else if (e.value > 50 && e.value <= 80 || e.value > 120 && e.value <= 150)
+                                            customClass += " bg-indigo-500"
+                                        else if (e.value >= 20 && e.value <= 50 || e.value > 150 && e.value <= 180)
+                                            customClass += " bg-red-300"
+                                        else
+                                            customClass += " bg-red-500"
+
+                                        return (
+                                            <div className={"flex items-center my-2"} key={e.name + index}>
+                                                <h3 className="text-base text-gray-600"
+                                                    style={{ width: "50%" }}>{e.name}</h3>
+                                                <div className='flex items-center h-6 bg-gray-200 ' style={{ width: "30%", borderRadius: "25px" }}>
+                                                    <div className={customClass} style={{ width: e.value, borderRadius: "25px" }}></div>
+                                                    <div className=' bg-gray-200 h-6' style={{ width: 100 - e.value, borderRadius: "25px" }}></div>
+                                                </div>
+                                                <div className={"flex justify-center"} style={{ width: "20%" }}>
+                                                    <IonIcon icon={arrowDownCircleOutline} />
+                                                </div>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
 
 
-                        <div className="bg-white rounded-xl p-7 mb-5">
-                            <h2 className='font-bold text-2xl mb-10'>Health score</h2>
+                            </div>
+                        </Container>
+                        <Container>
+                            <div className='flex justify-start items-center mb-6'>
+                                <IonIcon icon={trendingUp} className='mr-2' />
+                                <h2 className="text-2xl font-bold text-gray-800">Trend</h2>
 
 
-                            <div className='flex justify-center mr-10'>
-                                <LineChart
+                            </div>
+
+
+                            <div className="bg-white rounded-xl p-7 mb-5">
+                                <h2 className='font-bold text-2xl mb-10'>Health score</h2>
+
+
+                                <div className='flex justify-center mr-10'>
+                                    <LineChart
+                                        width={250}
+                                        height={200}
+                                        data={data}
+                                        margin={{
+                                            top: 5,
+                                            right: 0,
+                                            left: 0,
+                                            bottom: 5,
+                                        }}
+                                    >
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="name" />
+                                        <YAxis />
+                                        <Tooltip />
+                                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                                    </LineChart>
+                                </div>
+                            </div>
+                            <div className="bg-white rounded-xl p-7">
+                                <h2 className='font-bold text-2xl mb-10'>Macronutrients</h2>
+
+                                <AreaChart
                                     width={250}
                                     height={200}
-                                    data={data}
+                                    data={data2}
+                                    stackOffset="expand"
                                     margin={{
-                                        top: 5,
+                                        top: 10,
                                         right: 0,
                                         left: 0,
-                                        bottom: 5,
+                                        bottom: 0,
                                     }}
                                 >
                                     <CartesianGrid strokeDasharray="3 3" />
-                                    <XAxis dataKey="name" />
-                                    <YAxis />
-                                    <Tooltip />
-                                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                                </LineChart>
+                                    <XAxis dataKey="day" />
+                                    <YAxis tickFormatter={toPercent} />
+                                    <Area type="monotone" dataKey="a" stackId="1" stroke="#8884d8" fill="#8884d8" />
+                                    <Area type="monotone" dataKey="b" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+                                    <Area type="monotone" dataKey="c" stackId="1" stroke="#ffc658" fill="#ffc658" />
+                                </AreaChart>
                             </div>
-                        </div>
-                        <div className="bg-white rounded-xl p-7">
-                            <h2 className='font-bold text-2xl mb-10'>Macronutrients</h2>
-
-                            <AreaChart
-                                width={250}
-                                height={200}
-                                data={data2}
-                                stackOffset="expand"
-                                margin={{
-                                    top: 10,
-                                    right: 0,
-                                    left: 0,
-                                    bottom: 0,
-                                }}
-                            >
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="day" />
-                                <YAxis tickFormatter={toPercent} />
-                                <Area type="monotone" dataKey="a" stackId="1" stroke="#8884d8" fill="#8884d8" />
-                                <Area type="monotone" dataKey="b" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-                                <Area type="monotone" dataKey="c" stackId="1" stroke="#ffc658" fill="#ffc658" />
-                            </AreaChart>
-                        </div>
-                    </Container>
+                        </Container>
 
 
+                    </div>
                 </div>
-            </div>
-        </IonContent>
+            </IonContent>
+        </IonPage>
     );
 };
 

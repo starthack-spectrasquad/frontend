2b0b662e4cf5c677678a5781fab1465e39f9e22d
BenEliasMartin <martinbenelias@gmail.com>
2024-03-21 16:58:56 +0100

    changed food

--- src/pages/Recommendations.tsx
+++ src/pages/Recommendations.tsx
@@ -1,7 +1,7 @@
-import {IonContent, IonIcon, IonPage} from '@ionic/react';
+import { IonContent, IonIcon, IonPage } from '@ionic/react';
 import Container from '../components/Container';
 import Heading from "../components/Heading";
-import {bulb, arrowForwardCircleOutline, flame} from "ionicons/icons";
+import { bulb, arrowForwardCircleOutline, flame } from "ionicons/icons";
 
 const Recommendations: React.FC = () => {
     const insightData = [
@@ -20,80 +20,82 @@ const Recommendations: React.FC = () => {
 
     const recipeData = [
         {
-            title: "Nice Noodles",
+            title: "Optimize your morning cereal",
             subtitle: "View recipe",
-            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcel5pWrEwvMBYOILf_Cb0eZIsMF4alx2wYspunZYPFw&s",
-            image: "https://www.allrecipes.com/thmb/2rPJp4sRMmSa-5MgBRuHz8XDxlc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/233446-lo-mein-noodles-dmfs-2x1-1356-1490-62771eb9cd6840fdb75f332303fff7b1.jpg"
+            url: "https://images.unsplash.com/photo-1610441009633-b6ca9c6d4be2?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "https://images.unsplash.com/photo-1610441009633-b6ca9c6d4be2?q=80&w=2600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
-            title: "Nice Noodles",
+            title: "Reinvent your noodles",
             subtitle: "View recipe",
-            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcel5pWrEwvMBYOILf_Cb0eZIsMF4alx2wYspunZYPFw&s",
-            image: "https://www.allrecipes.com/thmb/2rPJp4sRMmSa-5MgBRuHz8XDxlc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/233446-lo-mein-noodles-dmfs-2x1-1356-1490-62771eb9cd6840fdb75f332303fff7b1.jpg"
+            url: "hhttps://images.unsplash.com/photo-1612927601601-6638404737ce?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            image: "https://images.unsplash.com/photo-1612927601601-6638404737ce?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
     ]
-  return (
+    return (
 
-          <IonPage >
-              <IonContent >
-                  <div className="bg-gray-100 h-fit min-h-full">
-                      <Heading name={"Recommendations"}/>
-              <Container >
-
-
-                  <div className={"flex gap-5 items-center my-5"}>
-                      <IonIcon icon={bulb}/>
-                      <div className="text-2xl font-semibold">Insights</div>
-                  </div>
+        <IonPage >
+            <IonContent >
+                <div className="bg-gray-100 h-fit min-h-full">
+                    <Heading name={"Recommendations"} />
+                    <Container >
 
 
-                  <div>
-                      {insightData.map((e: any) => {
-                          return (
-                              <div className={"py-3 gap-1 flex flex-col"}>
-                                  <span style={{lineHeight: "15pt"}}
-                                        className={"font-semibold text-lg"}>{e.title}</span>
-                                  <div onClick={() => {window.location.replace(e.url)}} className={"flex gap-6 items-center"}>
-                                      <span style={{color: "#9C7474"}}
-                                            className={"font-semibold text-base"}>{e.subtitle}</span>
-                                      <IonIcon style={{color: "#9C7474"}} icon={arrowForwardCircleOutline}/>
-                                  </div>
-                              </div>
-                          )
-                      })}
-                  </div>
+                        <div className={"flex gap-5 items-center my-5"}>
+                            <IonIcon icon={bulb} />
+                            <div className="text-2xl font-semibold">Insights</div>
+                        </div>
 
-                  <div className={"flex items-center gap-5 my-6"}>
-                      <IonIcon    icon={flame}/>
-                      <div className="text-2xl font-semibold">Recipes</div>
-                  </div>
-                  <div className="max-w-md mx-auto my-2 p-6 bg-white rounded-3xl flex flex-col gap-4">
-                      {recipeData.map((e: any) => {
-                          return (
-                              <div>
-                                  <div className="text-2xl font-semibold">{e.title}</div>
 
-                                  <img alt={".."}
-                                       src={e.image}/>
+                        <div>
+                            {insightData.map((e: any) => {
+                                return (
+                                    <div className={"py-3 gap-1 flex flex-col"}>
+                                        <span style={{ lineHeight: "15pt" }}
+                                            className={"font-semibold text-lg"}>{e.title}</span>
+                                        <div onClick={() => { window.location.replace(e.url) }} className={"flex gap-6 items-center"}>
+                                            <span style={{ color: "#9C7474" }}
+                                                className={"font-semibold text-base"}>{e.subtitle}</span>
+                                            <IonIcon style={{ color: "#9C7474" }} icon={arrowForwardCircleOutline} />
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
 
-                                  <div className={"flex gap-6 items-center"}>
-                                      <span onClick={() => {window.location.replace(e.url)}} style={{color: "#9C7474"}}
-                                            className={"font-semibold text-base"}>
-                                          {e.subtitle}
-                                      </span>
-                                      <IonIcon style={{color: "#9C7474"}} icon={arrowForwardCircleOutline}/>
-                                  </div>
-                              </div>
-                          )
-                      })}
+                        <div className={"flex items-center gap-5 my-6"}>
+                            <IonIcon icon={flame} />
+                            <div className="text-2xl font-semibold">Recipes</div>
+                        </div>
 
-                  </div>
+                        {recipeData.map((e: any) => {
+                            return (
+                                <div className="max-w-md mx-auto my-4 p-6 bg-white rounded-3xl flex flex-col gap-4">
+                                    <div>
+                                        <div className="text-2xl font-semibold">{e.title}</div>
 
-              </Container>
-                  </div>
-              </IonContent>
-          </IonPage>
-  );
+                                        <img alt={".."}
+                                            src={e.image} className='object-cover h-48 w-96 rounded-xl my-3' />
+
+                                        <div className={"flex gap-6 items-center"}>
+                                            <span onClick={() => { window.location.replace(e.url) }} style={{ color: "#9C7474" }}
+                                                className={"font-semibold text-base"}>
+                                                {e.subtitle}
+                                            </span>
+                                            <IonIcon style={{ color: "#9C7474" }} icon={arrowForwardCircleOutline} />
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+
+
+
+                    </Container>
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Recommendations;
